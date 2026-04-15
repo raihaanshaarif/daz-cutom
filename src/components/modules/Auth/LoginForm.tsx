b5fbc19d351a6828e936e9bdc6cd5169762b0562
raihaanshaarif <codexbd@gmail.com>
2026-04-15 17:52:15 +0600

    website reload issue fixed

--- src/components/modules/Auth/LoginForm.tsx
+++ src/components/modules/Auth/LoginForm.tsx
@@ -48,7 +48,7 @@ export default function LoginForm() {
     const result = await signIn("credentials", {
       ...values,
       callbackUrl: "/dashboard",
-      redirect: true,
+      redirect: false,
     });
 
     if (!result?.ok) {
--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -3,7 +3,9 @@
 import { SessionProvider } from "next-auth/react";
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider refetchOnWindowFocus={false}>{children}</SessionProvider>
+  );
 };
 
 export default AuthProvider;
